288a4d657d21eee7256ef2d26d21ba3af071a29e
fernandokonopacki <luisfernandokonopacki@gmail.com>
2024-08-27 23:44:33 -0300

    [INIT]: Iniciando o projeto

---
+++ src/main/java/com/fernandok/hmssrvgateway/HmsSrvGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fernandok.hmssrvgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsSrvGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsSrvGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandok/hmssrvgateway/HmsSrvGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandok.hmssrvgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsSrvGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
